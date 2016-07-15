29cd851f22c082569b0eb3e3a985201f19c7b484
cytiand <XiaoTian1290996409@163.com>
2016-07-15 11:04:04 +0800

    猫类

---
+++ src/scott/cat.java
@@ -0,0 +1,25 @@
+/**
+ * @Copyright:   Copyright(c) 2016 ISoftstone  Co. Ltd. All Rights Reserved.
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @Version:     1.0
+ *
+ * @author:      issuser
+ * @Date:        2016年7月15日
+ *
+ * @Modification History:
+ * [Editor] [date] [remark]
+ *
+ * @Review History:
+ * [Reviewer] [date] [remark]
+ *
+ */
+package scott;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * @author : issuser
+ * @date:   2016年7月15日
+ */
+public class cat {
+
+}
